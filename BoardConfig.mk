eeb8e08f6a9e6893bbe310ac80a4bcee4143ece9
Sean <seanmaas27@gmail.com>
2013-11-20 18:50:21 -0500

    initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,136 @@
+#
+# Copyright (C) 2013 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+-include vendor/samsung/kylessopen/BoardConfigVendor.mk
+
+# Platform
+BOARD_VENDOR := samsung
+
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a5
+TARGET_CPU_VARIANT := cortex-a5
+ARCH_ARM_HAVE_NEON := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_GLOBAL_CFLAGS += -mtune=cortex-a5 -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a5 -mfpu=neon -mfloat-abi=softfp
+
+TARGET_BOARD_PLATFORM := msm7x27a
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+# Kernel
+TARGET_KERNEL_CONFIG := cyanogenmod_kylessopen_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/kylessopen
+TARGET_BOOTLOADER_BOARD_NAME := kylessopen
+BOARD_KERNEL_CMDLINE := androidboot.hardware=kylessopen
+BOARD_KERNEL_BASE := 0x00200000
+BOARD_KERNEL_PAGESIZE := 4096
+
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/kylessopen/include
+
+# Filesystem
+BOARD_BOOTIMAGE_PARTITION_SIZE := 12582912
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 12582912
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1258291200
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1946804224
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+BOARD_VOLD_MAX_PARTITIONS := 24
+
+# Qualcomm hardware
+BOARD_USES_QCOM_HARDWARE := true
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+
+# Graphics
+BOARD_EGL_CFG := device/samsung/kylessopen/prebuilt/system/lib/egl/egl.cfg
+USE_OPENGL_RENDERER := true
+TARGET_QCOM_DISPLAY_VARIANT := legacy
+TARGET_NO_HW_VSYNC := true
+BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
+
+# OMX
+TARGET_QCOM_LEGACY_OMX := true
+TARGET_QCOM_MEDIA_VARIANT := legacy
+COMMON_GLOBAL_CFLAGS += -DQCOM_NO_SECURE_PLAYBACK
+
+# PowerHAL
+TARGET_USES_CM_POWERHAL := true
+
+# Camera
+BOARD_USES_PROPRIETARY_LIBCAMERA := true
+BOARD_NEEDS_MEMORYHEAPPMEM := true
+COMMON_GLOBAL_CFLAGS += -DICS_CAMERA_BLOB -DSAMSUNG_CAMERA_LEGACY -DNEEDS_VECTORIMPL_SYMBOLS
+
+# RIL
+BOARD_PROVIDES_LIBRIL := true
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/kylessopen/include
+
+# Wi-Fi
+BOARD_WLAN_DEVICE := ath6kl
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_ath6kl
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_ath6kl
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/ath6kl_sdio.ko"
+WIFI_DRIVER_MODULE_NAME := "ath6kl_sdio"
+WIFI_EXT_MODULE_PATH := "/system/lib/modules/cfg80211.ko"
+WIFI_EXT_MODULE_NAME := "cfg80211"
+WIFI_TEST_INTERFACE := "sta"
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WIFI_DRIVER_FW_PATH_AP  := "ap"
+WIFI_DRIVER_FW_PATH_P2P := "p2p"
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/kylessopen/bluetooth
+
+# Audio
+TARGET_PROVIDES_LIBAUDIO := true
+
+# Lights
+TARGET_PROVIDES_LIBLIGHTS := true
+
+# FM Radio
+#BOARD_HAVE_QCOM_FM := true
+#COMMON_GLOBAL_CFLAGS += -DQCOM_FM_ENABLED
+
+# GPS
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := msm7x27a
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+
+# Bootanimation
+TARGET_NO_INITLOGO := true
+TARGET_BOOTANIMATION_USE_RGB565 := true
+
+# Recovery
+TARGET_RECOVERY_INITRC := device/samsung/kylessopen/recovery/init.rc
+TARGET_RECOVERY_FSTAB := device/samsung/kylessopen/ramdisk/fstab.qcom
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/kylessopen/recovery/recovery_keys.c
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+BOARD_UMS_LUNFILE := "/sys/devices/platform/msm_hsusb/gadget/lun%d/file"
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_HAS_SDCARD_INTERNAL := trues
+BOARD_HAS_DOWNLOAD_MODE := true
+BOARD_USES_MMCUTILS := true
+BOARD_HAS_NO_MISC_PARTITION := true
