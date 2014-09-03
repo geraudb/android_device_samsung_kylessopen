6578600743ab33ebc6880fb46dd14a29471554c4
geraud <android@fandb.ca>
2014-09-03 12:37:14 -0400

    Alligning with jonypx09....part 2
    Change-Id: I823e9314a4a739c5f5f93a370307a79de0547844

--- kylessopen.mk
+++ kylessopen.mk
@@ -39,7 +39,10 @@ PRODUCT_PACKAGES += \
     gralloc.msm7x27a \
     hwcomposer.msm7x27a \
     libtilerenderer
-
+    
+# Torch
+#PRODUCT_PACKAGES += Torch
+    
 # Lights
 PRODUCT_PACKAGES += \
     lights.msm7x27a
@@ -66,6 +69,14 @@ PRODUCT_COPY_FILES += \
 # Prebuilt
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,device/samsung/kylessopen/prebuilt/system,system)
+    
+# Classic Webview
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.webview.provider=classic
+    
+#Optimization for low-memory devices
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.low_ram=true 
 
 # Hardware features available on this device
 PRODUCT_COPY_FILES += \
@@ -87,6 +98,16 @@ PRODUCT_COPY_FILES += \
     device/samsung/kylessopen/recovery/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh \
     device/samsung/kylessopen/recovery/postrecoveryboot.sh:recovery/system/bin/postrecoveryboot.sh
 
+# FM
+PRODUCT_COPY_FILES += \
+    device/samsung/kylessopen/prebuilt/system/etc/init.qcom.fm.sh:/system/etc/init.qcom.fm.sh
+
+# FM Config
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.fm.analogpath.supported=true \
+    ro.fm.transmitter=false \
+    ro.fm.mulinst.recording.support=false
+    
 # Enable repeatable keys in CWM
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.cwm.enable_key_repeat=true
