8c735bfc8f3254d4b3b9bf8dd3dae7aebb6b4682
geraud <android@fandb.ca>
2014-09-23 18:02:00 -0400

    Fix path mgmt during patching process Try to add USB mass storage back, but it is not working yet....
    Change-Id: Id2cd22034509c2f0da05594a5a2f41ec225186ec

--- apply-patches.sh
+++ apply-patches.sh
@@ -9,6 +9,7 @@ for i in $(find "$PATCHBASE"/* -type d); do
 	PATCHTARGET=$PATCHNAME
 	for i in $(seq 4); do
 		PATCHTARGET=$(echo $PATCHTARGET | sed 's/_/\//')
+                PATCHTARGET=$(echo $PATCHTARGET | sed 's/-/_/')
 		if [ -d "$CMBASE/$PATCHTARGET" ]; then break; fi
 	done
 	echo applying $PATCHNAME to $PATCHTARGET
